d9cebf99a0b98c9e40fc0c9b3d1d78aeedfd186b
Nosach Liliya <mooduck@icloud.com>
2022-12-26 21:52:39 +0600

    изменения

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,51 +1,7 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Main {
-
-    public static final String URI = "https://api.nasa.gov/planetary/apod?api_key=O4XNz8c3EqCWKsPLhg92zzPuBVs7KiXu7FxRor10";
-
-    public static final ObjectMapper mapper = new ObjectMapper();
-
-    public static void main(String[] args) throws IOException {
-
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setDefaultRequestConfig(RequestConfig.custom()
-                        .setConnectTimeout(5000)
-                        .setSocketTimeout(30000)
-                        .setRedirectsEnabled(false)
-                        .build())
-                .build();
-
-        CloseableHttpResponse response = httpClient.execute(new HttpGet(URI));
-
-        NasaObject nasaObject = mapper.readValue(response.getEntity().getContent(), NasaObject.class);
-        System.out.println(nasaObject);
-
-        CloseableHttpResponse pictureResponse = httpClient.execute(new HttpGet(nasaObject.getUrl()));
-
-        String[] arr = nasaObject.getUrl().split("/");
-        String fileName = arr[arr.length - 1];
-
-        HttpEntity entity = pictureResponse.getEntity();
-        if (entity != null) {
-            FileOutputStream fos = new FileOutputStream(fileName);
-            entity.writeTo(fos);
-            fos.close();
-        } else {
-            System.out.println("Объект null");
-        }
-
-
-
-
+    public static void main(String[] args) throws TelegramApiException {
+        new MyTelegramBot();
     }
 }
